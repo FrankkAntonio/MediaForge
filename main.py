d02d8a9d68626c55c80c49535dfc35342f6cd695
FrankkAntonio <frankkantonio69@gmail.com>
2026-09-01 14:57:22 -0300

    refactor: improve media downloader

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.mediaforge.downloader import mediaDowloader
+from src.mediaforge.downloader import MediaDowloader
 
 def main():
     url = input("Digite o link da sua URL: ")
 
-    downloader = mediaDowloader()
+    downloader = MediaDowloader()   
     downloader.download_video(url)
 
     print("\nDownload completo!")
--- src/mediaforge/downloader.py
+++ src/mediaforge/downloader.py
@@ -1,6 +1,6 @@
 import yt_dlp
 
-class mediaDowloader:
+class MediaDowloader:
 
     def __init__(self, output_path="downloads"):
         self.output_path = output_path
@@ -8,6 +8,7 @@ class mediaDowloader:
     def download_video(self, url):
         options = {
             "outtmpl": f"{self.output_path}/%(title)s.%(ext)s",
+            "format": "bestvideo+bestaudio/best",
         }
         with yt_dlp.YoutubeDL(options) as ydl:
             ydl.download([url])
